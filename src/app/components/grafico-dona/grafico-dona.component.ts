725f530e853ebe5ce2ba75205cc256c178dc138a
IsidroVale <isiperez1502@hotmail.com>
2019-04-16 10:54:00 -0400

    Input, output y viewchild

---
+++ src/app/components/grafico-dona/grafico-dona.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './grafico-dona.component.html',
+  styleUrls: ['./grafico-dona.component.css']
+})
+export class GraficoDonaComponent implements OnInit {
+
+      // Doughnut
+      @Input() public ChartLabels: string[] = [];
+      @Input() public ChartData: number[] = [];
+      @Input() public ChartType: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from "@angular/core";
 
+import { FormsModule } from '@angular/forms';
+
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
+
+//ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+
+//temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
@@ -13,7 +24,9 @@ import { PAGES_ROUTES } from './pages.routes';
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent
   ],
   exports: [
     DashboardComponent,
@@ -22,7 +35,9 @@ import { PAGES_ROUTES } from './pages.routes';
   ],
   imports:[
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
   ]
 })
 
